46ff6b12e970f6abdfbef42d48ba40fa3b248916
VanessaTorres004 <vanessa.torres.chacon@udla.edu.ec>
2024-12-01 20:33:09 -0500

    cambios en el about page en shell y generales

--- Views/AllNotesPage.xaml.cs
+++ Views/AllNotesPage.xaml.cs
@@ -23,13 +23,13 @@ public partial class AllNotesPage : ContentPage
     {
         if (e.CurrentSelection.Count != 0)
         {
-            // Get the note model
+            
             var note = (Models.Note)e.CurrentSelection[0];
 
-            // Should navigate to "NotePage?ItemId=path\on\device\XYZ.notes.txt"
+           
             await Shell.Current.GoToAsync($"{nameof(NotePage)}?{nameof(NotePage.ItemId)}={note.Filename}");
 
-            // Unselect the UI
+            
             notesCollection.SelectedItem = null;
         }
     }
